6a11841b049c6a264ee027cb8a1afe61cfe7b195
D Stephan <dstepha3@kent.edu>
2021-08-14 22:01:46 -0400

    initial set up & universal header creation

--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
+// IMPORT STYLESHEETS
+import "./assets/navigation.css";
+import "./assets/style.css";
+
 import { createApp } from 'vue'
 import App from './App.vue'
 
+
 createApp(App).mount('#app')
+
+
+
+
+
---
+++ src/views/Home.vue
@@ -0,0 +1,11 @@
+<template lang="html">
+    <h1>This is the Home Page</h1>
+</template>
+
+<script lang="js">
+
+export default {
+    name: 'home'
+}
+
+</script>
