60b0ecb36ff1f7fd83da37a6a211635d4ed0af3e
D Stephan <dstepha3@kent.edu>
2021-08-15 03:07:26 -0400

    mobile styling

--- src/views/Music.vue
+++ src/views/Music.vue
@@ -1,7 +1,7 @@
 <template lang="html">
     <div id="music-body">
         <h1 style="margin-bottom: 80px; padding: 0 40px 10px; border-bottom: 1px solid var(--theme-primary-dark);">Hear the Latest</h1>
-        <div style="width: 50%">
+        <div class="soundcloud-player">
             <sc_player1></sc_player1>
         </div>
     </div>
