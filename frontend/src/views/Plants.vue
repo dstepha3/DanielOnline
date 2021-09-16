664db235344b35ae0d85dcaa8e4b46986edb0f4b
D Stephan <dstepha3@kent.edu>
2021-09-15 22:44:39 -0400

    plant img

--- frontend/src/views/Plants.vue
+++ frontend/src/views/Plants.vue
@@ -3,6 +3,7 @@
     <NavBar></NavBar>
 
     <div id="body" class="fade-in">
+
         <!-- CTA -->
         <div class="row cta" style="margin-top: 4% !important; margin-bottom: 4% !important">
             <h3>Welcome to the main Plant Landing</h3>
@@ -13,6 +14,14 @@
             </router-link>
         </div>
 
+        
+
+
+    </div>
+
+    <div class="img-container">
+        <img class="image" src="https://i.imgur.com/aCRP3jj.jpg">
+        <div class="overlay"></div>
     </div>
 
     <FooterLarge></FooterLarge>
@@ -32,3 +41,23 @@ export default {
 }
 
 </script>
+
+<style scoped>
+.image{
+    width: 100%;
+    position: relative;
+}
+.overlay{
+    background: linear-gradient(180deg, rgba(13,13,13,1) 1%, rgba(255,255,255,0) 100%);     
+    position: absolute;
+    top: 0;
+    content: '';
+    z-index: 99;
+    height: 400px;
+    width: 100%;
+}
+.img-container{
+    position: relative;
+    margin-top: 60px;
+}
+</style>
