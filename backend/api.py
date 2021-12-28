60280eb17343f11cf41b8793986f8ec348805691
D Stephan <dstepha3@kent.edu>
2021-12-27 21:38:15 -0500

    removing API dependence

--- backend/api.py
+++ backend/api.py
@@ -2,27 +2,7 @@
 from flask import jsonify
 from app import app
 
-from plants import Plants
-from courses import KentCoreCourses, MajorCourses
-
 # sanity check route
 @app.route('/ping', methods=['GET'])
 def ping_pong():
     return jsonify('pong!')
-
-
-#########     Plant Routes      #########
-@app.route('/api/plants', methods=['GET'])
-def my_plants():
-    return jsonify(Plants)
-
-
-#########      Course Routes      #########
-@app.route('/api/courses/kentcore', methods=['GET'])
-def kentCore():
-    return jsonify(KentCoreCourses)
-
-
-@app.route('/api/courses/majorcourses', methods=['GET'])
-def majorCourses():
-    return jsonify(MajorCourses)
