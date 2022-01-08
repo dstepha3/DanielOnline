5c19f1c160f28705816d787c8588dc480dd8fc89
D Stephan <dstepha3@kent.edu>
2022-01-08 14:08:12 -0500

    + Update form handler

--- frontend/src/views/pages/Contact.vue
+++ frontend/src/views/pages/Contact.vue
@@ -4,15 +4,15 @@
         <h1 class="mb-3">Contact Me</h1>
         <p class="mb-5">Fill out the form below and I will be in touch as soon as I can.</p>
             <div class="form-container align-center">
-          <form class="contact-form" @submit.prevent="sendEmail">
+          <form class="contact-form" action="https://public.herotofu.com/v1/94eac200-70b4-11ec-b2d5-af79f33d926c" method="post">
             <div class="mb-4">
-                <input type="text" class="form-control" name="users-name" placeholder="John Doe">
+                <input type="text" class="form-control" name="Name" id="name" placeholder="John Doe">
             </div>
             <div class="mb-4">
-                <input type="email" class="form-control" name="users-email" placeholder="name@example.com">
+                <input type="email" class="form-control" name="Email" id="email" placeholder="name@example.com">
             </div>
             <div class="mb-5">
-                <textarea class="form-control" name="users-message" placeholder="message" rows="5"></textarea>
+                <textarea class="form-control" name="Message" id="message" placeholder="message" rows="7"></textarea>
             </div>
             <div style="display: flex; justify-content: space-between;">
               <input type="reset" class="btn" style="color: var(--theme-white); background: var(--theme-black); width: 150px;"/>
@@ -28,31 +28,18 @@
 
 import NavBar from "@/components/NavBar.vue";
 import FooterLarge from "@/components/Footer-Large.vue";
-import emailjs from 'emailjs-com';
-import { init } from 'emailjs-com';
-import router from '@/router/index.js';
+// import emailjs from 'emailjs-com';
+// import { init } from 'emailjs-com';
+// import router from '@/router/index.js';
 
-init("user_vsho8JdW4BokewL2D4klp");
+// init("user_vsho8JdW4BokewL2D4klp");
 
 export default {
     name: 'contact',
     components: {
         NavBar,
         FooterLarge
-    },
-    methods: {
-    sendEmail: (e) => {
-      emailjs.sendForm('dstephan316', 'template_contact_donline', e.target, 'user_vsho8JdW4BokewL2D4klp')
-        .then((result) => {
-            console.log('SUCCESS!', result.status, result.text);
-            document.body.style.cursor='default';
-            router.push({name: "ContactThankYou"});
-        }, (error) => {
-            document.body.style.cursor='default';
-            console.log('FAILED...', error);
-        });
     }
-  }
 }
 
 </script>
